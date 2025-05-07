b4c2f3caaa70af226d579535e416868e3d42217e
Robert McLay <mclay@zaniahgroup.com>
2025-05-07 16:20:43 -0500

    clean env from Lmod env vars

--- rt/common_funcs.sh
+++ rt/common_funcs.sh
@@ -3,6 +3,17 @@ init()
   eval $($projectDir/tools/cleanEnv)
   ORIG_HOME=$HOME
   HOME=$outputDir
+
+  while IFS='=' read -r name value; do
+    if [ "$name" = "LMOD_CMD" ] || [ "$name" = "LMOD_DIR" ]; then
+        :
+    elif [[ "$name" =~ ^__LMOD_REF_COUNT.* ]]; then
+        unset $name
+    elif [[ "$name" =~ ^LMOD.* ]]; then
+        unset $name
+    fi
+  done < <(env)
+
   export LMOD_RTM_TESTING=1
 
   rm -rf _stderr.orig
