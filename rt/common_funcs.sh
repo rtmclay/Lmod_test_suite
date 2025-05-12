77f4ebd72f207ab763aab41cd635489ea501df6f
Robert McLay <mclay@zaniahgroup.com>
2025-05-12 18:43:22 -0500

    clearing MT from init

--- rt/common_funcs.sh
+++ rt/common_funcs.sh
@@ -1,5 +1,6 @@
 init()
 {
+  unsetMT
   eval $($projectDir/tools/cleanEnv)
   ORIG_HOME=$HOME
   HOME=$outputDir
@@ -31,4 +32,40 @@ cleanUp ()
        -e "s|$HOME|~|g"                                   \
        -e "/^ *$/d"                                       \
        < $1 > $2
+}
+unsetMT ()
+{
+   unset _ModuleTable_
+   local last
+   last=1000
+   if [ -n "$_ModuleTable_Sz_" ]; then
+       last=$_ModuleTable_Sz_
+       unset _ModuleTable_Sz_
+   fi
+   for ((i=1; i<=last; i++)); do
+      num=`printf %03d $i`
+      eval j="\$_ModuleTable${num}_"
+      if [ -z "$j" ]; then
+         break
+      fi
+      unset _ModuleTable${num}_
+   done
+
+   if [ -n $_ModuleTable_Sz_ ]; then
+       unset _ModuleTable_Sz_
+   fi
+   last=1000
+   for ((i=1; i<=last; i++)); do
+      num=`printf %03d $i`
+      eval j="\$_ModuleTable_${num}_"
+      if [ -z "$j" ]; then
+         break
+      fi
+      unset _ModuleTable_${num}_
+   done
+   while IFS='=' read -r name value ; do
+     if [[ $name =~ __LMOD_REF_COUNT_ ]]; then
+       unset $name
+     fi
+   done < <(env)
 }
